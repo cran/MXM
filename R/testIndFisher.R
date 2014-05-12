f4d6fd681fa287a5d834683e84052349b990b271
Giorgos Athineou <athineou@ics.forth.gr>
2014-05-12 18:19:24 +0000

    version 0.1

---
+++ R/testIndFisher.R
@@ -0,0 +1,197 @@
+testIndFisher = function(target, dataset, xIndex, csIndex, dataInfo=NULL, univariateModels=NULL , hash = FALSE, stat_hash=NULL, pvalue_hash=NULL)
+{
+  # TESTINDFISHER Fisher Conditional Independence Test for continous class variables
+  # PVALUE = TESTINDFISHER(Y, DATA, XINDEX, CSINDEX, DATAINFO)
+  # This test provides a p-value PVALUE for the NULL hypothesis H0 which is
+  # X is independent by TARGET given CS. The pvalue is calculated following
+  # Fisher's method (see reference below)
+  
+  # This method requires the following inputs
+  #   TARGET: a numeric vector containing the values of the target variable.
+  #   DATASET: a numeric data matrix containing the variables for performing the test.
+  #   XINDEX: the index of the variable whose association with the target we want to test.
+  #   CSINDEX: the indices if the variable to condition on.
+  #   DATAINFO: information on the structure of the data
+  
+  # this method returns: the pvalue PVALUE, the statistic STAT and a control variable FLAG.
+  # if FLAG == 1 then the test was performed succesfully
+  
+  # References
+  # [1] Peter Spirtes, Clark Glymour, and Richard Scheines. Causation,
+  # Prediction, and Search. The MIT Press, Cambridge, MA, USA, second
+  # edition, January 2001.
+  
+  # Copyright 2012 Vincenzo Lagani and Ioannis Tsamardinos
+  # R Implementation by Giorgos Athineou (10/2013)
+  
+  
+  #########################################################################################################
+  
+  #initialization
+  
+  #if the test cannot performed succesfully these are the returned values
+  pvalue = 1;
+  stat = 0;
+  flag = 0;
+  
+  csIndex[which(is.na(csIndex))] = 0;
+  
+  if(hash == TRUE)
+  {
+    csIndex2 = csIndex[which(csIndex!=0)]
+    csindex2 = sort(csIndex2)
+    xcs = c(xIndex,csIndex2)
+    key = paste(as.character(xcs) , collapse=" ");
+    if(is.null(stat_hash[[key]]) == FALSE)
+    {
+      stat = stat_hash[[key]];
+      pvalue = pvalue_hash[[key]];
+      flag = 1;
+      
+      results <- list(pvalue = pvalue, stat = stat, flag = flag , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+      return(results);
+    }
+  }
+  
+  #if the xIndex is contained in csIndex, x does not bring any new
+  #information with respect to cs
+  if(!is.na(match(xIndex,csIndex)))
+  {
+    if(hash == TRUE)#update hash objects
+    {
+      .set(stat_hash , key , 0)
+      .set(pvalue_hash , key , 1)
+    }
+    results <- list(pvalue = 1, stat = 0, flag = 1 , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+    return(results);
+  }
+  
+  #check input validity
+  if(xIndex < 0 || csIndex < 0)
+  {
+    message(paste("error in testIndFisher : wrong input of xIndex or csIndex"))
+    results <- list(pvalue = pvalue, stat = stat, flag = flag , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+    return(results);
+  }
+  
+  xIndex = unique(xIndex);
+  csIndex = unique(csIndex);
+  
+  #extract the data
+  x = dataset[ , xIndex];
+  cs = dataset[ , csIndex];
+  
+  #checking the length
+  if (length(x) == 0 || length(target) == 0)
+  {
+    message(paste("error in testIndFisher : empty variable x or target"))
+    results <- list(pvalue = pvalue, stat = stat, flag = flag , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+    return(results);
+  }
+  
+  #if x or target is constant then there is no point to perform the test
+  if(var(x) == 0 || var(target) == 0)
+  {
+    if(hash == TRUE)#update hash objects
+    {
+      .set(stat_hash , key , 0)
+      .set(pvalue_hash , key , 1)
+    }
+    results <- list(pvalue = 1, stat = 0, flag = 1 , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+    return(results);
+  }
+  
+  #remove constant columns of cs
+  cs = as.matrix(cs)
+  cs = cs[,apply(cs, 2, var, na.rm=TRUE) != 0]
+  
+  #trycatch for dealing with errors
+  res <- tryCatch(
+{
+  #if the conditioning set (cs) is empty, we use a simplified formula
+  if(length(cs) == 0)
+  {
+    if(!is.null(univariateModels))
+    {
+      pvalue = univariateModels$pvalue[[xIndex]];
+      stat = univariateModels$stat[[xIndex]];
+      flag = univariateModels$flag[[xIndex]];
+      results <- list(pvalue = pvalue, stat = stat, flag = flag , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+      return(results);
+    }
+    
+    #compute the correlation coefficient between x,target directly
+    stat = abs(cor(x,target));
+  }else{
+    #perform the test with the cs
+    tmpm = cbind(x,target,cs);
+    
+    corrMatrix = cor(tmpm);
+    
+    xyIdx = 1:2;
+    csIdx = 3:(ncol(as.matrix(cs))+2); #or csIdx = 3;
+    
+    residCorrMatrix = (corrMatrix[xyIdx, xyIdx]) - as.matrix(corrMatrix[xyIdx, csIdx])%*%(solve( as.matrix(corrMatrix[csIdx, csIdx]) , rbind(corrMatrix[csIdx, xyIdx]) ));
+    #residCorrMatrix = (corrMatrix[xyIdx, xyIdx]) - as.matrix(corrMatrix[xyIdx, csIdx])%*%(mldivide(as.matrix(corrMatrix[csIdx, csIdx]),rbind(corrMatrix[csIdx, xyIdx]) ));
+    
+    stat = abs(residCorrMatrix[1,2] / sqrt(residCorrMatrix[1,1] * residCorrMatrix[2,2]));
+  }
+  
+  #lets calculate the p-value
+  
+  #comparing against the Student's t distribution
+  z = 0.5*log((1+stat)/(1-stat));
+  df = length(x) - ncol(as.matrix(cs)) - 3; #degrees of freedom
+  w = sqrt(df)*z;
+  
+  pvalue = 2 * pt(-abs(w),df);#?dt for documentation
+  #or alternatively we can calculate the p-value by comparing against the normal distribution
+  #pvalue = 2 * dnorm(-abs(w),mean=0,sd=1);
+  
+  flag = 1;
+  
+  #last error check
+  if(is.na(pvalue) || is.na(stat))
+  {
+    pvalue = 1;
+    stat = 0;
+    flag = 0;
+  }else{
+    #update hash objects
+    if(hash == TRUE)
+    {
+      .set(stat_hash , key , stat)
+      .set(pvalue_hash , key , pvalue)
+    }
+  }
+  
+  #testerrorcaseintrycatch(4);
+  
+  results <- list(pvalue = pvalue, stat = stat, flag = flag , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+  return(results);
+  
+},
+error=function(cond) {
+  message(paste("error in try catch of the testIndFisher test"))
+  message("Here's the original error message:")
+  message(cond)
+  stop();
+  #        
+  #        #for debug
+  #        printf("\nxIndex = \n");
+  #        print(xIndex);
+  #        printf("\ncsindex = \n");
+  #        print(csIndex);
+  
+  #error case
+  pvalue = 1;
+  stat = 0;
+  flag = 0;
+  
+  results <- list(pvalue = pvalue, stat = stat, flag = flag , stat_hash=stat_hash, pvalue_hash=pvalue_hash);
+  return(results);
+},
+finally={}
+  )    
+  return(res);
+}
